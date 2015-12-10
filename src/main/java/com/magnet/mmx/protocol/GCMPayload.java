b1e9e65a29f37b2cb0441f392e4da5caf8a5feb5
Vincent Lau <vincent.lau@magnet.com>
2015-12-10 09:52:01 -0800

    Just update the javadoc.

--- src/main/java/com/magnet/mmx/protocol/GCMPayload.java
+++ src/main/java/com/magnet/mmx/protocol/GCMPayload.java
@@ -20,9 +20,9 @@ import java.io.Serializable;
 import java.util.Map;
 
 /**
- * GCM push payload from client to client.  The mmx dictionary contains at least
+ * Push payload from client to client.  The mmx dictionary contains at least
  * {@link #KEY_CALLBACK_URL}, {@link #KEY_CUSTOM_CONTENT}, {@link #KEY_PUSH_ID},
- * {@link #KEY_TYPE} properties.  
+ * {@link #KEY_TYPE} properties.
  */
 public class GCMPayload implements Serializable {
   private static final long serialVersionUID = 1678052400350808468L;
@@ -56,50 +56,107 @@ public class GCMPayload implements Serializable {
   @SerializedName(Constants.PAYLOAD_MMX_KEY)
   private Map<String, ? super Object> mmx;
 
+  /**
+   * Get the optional title.
+   * @return A title or null.
+   */
   public String getTitle() {
     return title;
   }
 
+  /**
+   * Set the title.
+   * @param title Title text, or null.
+   */
   public void setTitle(String title) {
     this.title = title;
   }
 
+  /**
+   * Get the optional content text.
+   * @return Content text, or null.
+   */
   public String getBody() {
     return body;
   }
 
+  /**
+   * Set the content text.
+   * @param body Content text.
+   */
   public void setBody(String body) {
     this.body = body;
   }
 
+  /**
+   * Get the optional icon name.  The icon name will be mapped to an icon
+   * resource ID in the target Android application.
+   * @return An icon name, or null.
+   */
   public String getIcon() {
     return icon;
   }
 
+  /**
+   * Set the icon to be displayed in the notification.
+   * @param icon The icon resource name, or null.
+   */
   public void setIcon(String icon) {
     this.icon = icon;
   }
 
+  /**
+   * Get the optional sound name.  Currently the custom sound is not
+   * implemented in Android, but used to enable/disable default notification
+   * ring-tone.
+   * @return A sound name or null.
+   */
   public String getSound() {
     return sound;
   }
 
+  /**
+   * Set the sound name to be used for notification.  In Android, the value is
+   * ignored but used to enable/disable the default notification ring-tone.
+   * @param sound A sound name or null.
+   */
   public void setSound(String sound) {
     this.sound = sound;
   }
 
+  /**
+   * Get the optional badge (iOS) or number at the right-hand side of the
+   * notification (Android.)
+   * @return A numeric value or null.
+   */
   public Integer getBadge() {
     return badge;
   }
 
+  /**
+   * Set a badge in iOS or a number at the right-hand side of the notification
+   * in Android.
+   * @param badge A numeric value or null.
+   */
   public void setBadge(Integer badge) {
     this.badge = badge;
   }
 
+  /**
+   * Get an optional dictionary encapsulating non-UI properties.  Some of the
+   * properties are created by the MMX Push subsystem.
+   * @return A dictionary or null.
+   */
   public Map<String, ? super Object> getMmx() {
     return mmx;
   }
 
+  /**
+   * @hide
+   * Set a dictionary encapsulating non-UI properties.  It is intended for
+   * internal use.
+   * @param mmx A dictionary.
+   */
   public void setMmx(Map<String, ? super Object> mmx) {
     this.mmx = mmx;
   }
