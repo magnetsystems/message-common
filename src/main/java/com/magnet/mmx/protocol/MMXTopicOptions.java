6e6c252ddddf328513e58e7898e810ebbaf9e679
Vincent Lau <vincent.lau@magnet.com>
2016-03-09 22:58:39 -0800

    MAX-285: add subscriber white-list for private channel.
    Allow the owner to specify the subscriber white-list when a
    private channel is created.

--- src/main/java/com/magnet/mmx/protocol/MMXTopicOptions.java
+++ src/main/java/com/magnet/mmx/protocol/MMXTopicOptions.java
@@ -1,4 +1,4 @@
-/*   Copyright (c) 2015 Magnet Systems, Inc.
+/*   Copyright (c) 2015-2016 Magnet Systems, Inc.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -19,18 +19,21 @@ import com.google.gson.annotations.SerializedName;
 import com.magnet.mmx.protocol.TopicAction.PublisherType;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
- * The topic options.  If no options are specified, the topic is defaulted to
- * authorized access (owner or subscribers), published by anyone, one 
- * persistent published item and subscription is enabled.
+ * The topic options.  If no options are specified, global topics are defaulted
+ * with anyone access, published by anyone, unlimited persistent published
+ * items, and subscription is enabled.  User topics are defaulted with whitelist
+ * access, published by subscribers, unlimited persistent published items, and
+ * subscription is enabled.
  * TODO: Review the definition of PublisherType after roles are introduced.
  */
 public class MMXTopicOptions implements Serializable {
   private static final long serialVersionUID = 5092103177218064923L;
   private static final Integer DEFAULT_MAX_ITEMS = -1;
   private static final PublisherType DEFAULT_PUB_TYPE = PublisherType.anyone;
-  
+
   @SerializedName("maxItems")
   private Integer mMaxItems;
   @SerializedName("publisherType")
@@ -41,6 +44,8 @@ public class MMXTopicOptions implements Serializable {
   private String mDescription;
   @SerializedName("subscribeOnCreate")
   private boolean mSubscribeOnCreate;
+  @SerializedName("whitelist")
+  private List<String> mWhiteList;
 
   /**
    * Get the publisher type.  Default will be {@link PublisherType#anyone}.
@@ -49,7 +54,7 @@ public class MMXTopicOptions implements Serializable {
   public PublisherType getPublisherType() {
     return mPublisherType;
   }
-  
+
   /**
    * Set the publisher type.  The publisher type states who can publish an
    * item to a topic.
@@ -68,7 +73,7 @@ public class MMXTopicOptions implements Serializable {
   public Boolean isSubscriptionEnabled() {
     return mSubscriptionEnabled;
   }
-  
+
   /**
    * Allow this topic to be subscribed or not.  Default is true.
    * @param enable True to enable; false to disable.
@@ -78,7 +83,7 @@ public class MMXTopicOptions implements Serializable {
     mSubscriptionEnabled = enable ? Boolean.TRUE : Boolean.FALSE;
     return this;
   }
-  
+
   /**
    * Get the maximum number of persistent items.
    * @return Max number of persistent items.
@@ -86,19 +91,19 @@ public class MMXTopicOptions implements Serializable {
   public Integer getMaxItems() {
     return mMaxItems;
   }
-  
+
   /**
    * Maximum number of items can be persisted.  If it is not set, the default
    * will be 1.  If the max number of items is 0, no published items will
    * be persisted.  The -1 means unlimited.
-   * @param maxItems Max number of published items to be persisted. 
+   * @param maxItems Max number of published items to be persisted.
    * @return This object.
    */
   public MMXTopicOptions setMaxItems(int maxItems) {
     mMaxItems = maxItems;
     return this;
   }
-  
+
   /**
    * Get an optional description of this topic.
    * @return The description of the topic, or null.
@@ -106,7 +111,7 @@ public class MMXTopicOptions implements Serializable {
   public String getDescription() {
     return mDescription;
   }
-  
+
   /**
    * Set an optional description to this topic.
    * @param description The description of the topic.
@@ -116,7 +121,7 @@ public class MMXTopicOptions implements Serializable {
     mDescription = description;
     return this;
   }
-  
+
   /**
    * Subscribe the topic for the creator when the topic is created.  It is a
    * user subscription, not an end-point subscription.
@@ -125,7 +130,7 @@ public class MMXTopicOptions implements Serializable {
   public boolean isSubscribeOnCreate() {
     return mSubscribeOnCreate;
   }
-  
+
   /**
    * For topic creation only that topic will be subscribed automatically.  This
    * option is not persisted.
@@ -136,7 +141,28 @@ public class MMXTopicOptions implements Serializable {
     mSubscribeOnCreate = subscribe;
     return this;
   }
-  
+
+  /**
+   * Get the white-list of subscribers if specified.  The list contains just
+   * the user ID's (not JID.)
+   * @return Null, empty list or a white-listed subscribers.
+   */
+  public List<String> getWhiteList() {
+    return mWhiteList;
+  }
+
+  /**
+   * Set the white-list of subscribers.  The list can be null or containing
+   * user ID's (not JID.)  Currently all user topics are created with whitelist
+   * access model.
+   * @param whiteList A list of white-listed subscribers, or null.
+   * @return This object.
+   */
+  public MMXTopicOptions setWhiteList(List<String> whiteList) {
+    mWhiteList = whiteList;
+    return this;
+  }
+
   /**
    * @hide
    * Fill the values with default values if they are not set.
